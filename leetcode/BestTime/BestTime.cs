644c9890a7650b074429596a9a41ecd44f98b13f
k1raz <100806295+k1raZ@users.noreply.github.com>
2023-07-22 18:24:38 +0000

    BestTime task

---
+++ leetcode/BestTime/BestTime.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode.BestTime;
+
+public class BestTime
+{
+    public int MaxProfit(int[] prices) {
+        if (prices == null || prices.Length < 2) {
+            return 0; // do not enough days, to make an order
+        }
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 1; i < prices.Length; i++) {
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.Min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
