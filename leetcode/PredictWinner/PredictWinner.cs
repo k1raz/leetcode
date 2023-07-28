c754620b731242778ae4c6439740a9a287c11e5c
k1raz <100806295+k1raZ@users.noreply.github.com>
2023-07-28 14:40:43 +0000

    changed

--- leetcode/PredictWinner/PredictWinner.cs
+++ leetcode/PredictWinner/PredictWinner.cs
@@ -2,23 +2,20 @@ namespace leetcode.MaximumRunning;
 
 public class PredictWinner
 {
-    public bool PredictTheWinner(int[] nums)
-    {
+    public bool PredictTheWinner(int[] nums) {
         int n = nums.Length;
         int[,] dp = new int[n, n];
-
-        for (int i = n - 1; i >= 0; i--)
-        {
+    
+        for (int i = 0; i < n; i++) {
             dp[i, i] = nums[i];
-            for (int j = i + 1; j < n; j++)
-            {
-                int a = nums[i] - dp[i + 1, j];
-                int b = nums[j] - dp[i, j - 1];
-                dp[i, j] = Math.Max(a, b);
+        }
+    
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i, j] = Math.Max(nums[i] - dp[i+1, j], nums[j] - dp[i, j-1]);
             }
         }
-
-        int totalSum = nums.Sum();
-        return dp[0, n - 1] >= (totalSum + 1) / 2;
+    
+        return dp[0, n - 1] >= 0;
     }
 }
